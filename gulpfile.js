18d9e51ebe6c7bd2ae6d0fc32787a3f0c075a56d
Smile_Rose <hey_cool@163.com>
2016-09-01 17:42:59 +0800

    wap

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@
  * step4: npm install gulp --save-dev  本地安装（生成node_modules文件夹）
  * step5 gulp -v 检测安装情况 （正常情况应该包含本地和全局两个信息）
  * *
- * $ npm install gulp-ruby-sass gulp-sass gulp-autoprefixer gulp-minify-css gulp-imagemin imagemin-pngquant gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-rev-append gulp-utf8-convert gulp-cache del  gulp-htmlmin  --save-dev
+ * $ npm install gulp-ruby-sass gulp-sass gulp-autoprefixer gulp-minify-css gulp-imagemin imagemin-pngquant gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-rev-append gulp-utf8-convert gulp-cache del  gulp-htmlmin  browser-sync --save-dev
  */
 // Load plugins
 //css
@@ -13,7 +13,7 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-   // jshint = require('gulp-jshint'),
+    // jshint = require('gulp-jshint'),
     //img
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant'),
@@ -26,36 +26,39 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     rev = require('gulp-rev-append'),
     livereload = require('gulp-livereload'),
-    //htmlmin = require('gulp-htmlmin'),
+    htmlmin = require('gulp-htmlmin'),
     del = require('del'),
     utf8Convert = require('gulp-utf8-convert');
-   // fileinclude = require('gulp-file-include'),// include 文件用
-    //  template = require('gulp-template'),//替换变量以及动态html用
+   var browserSync = require('browser-sync').create();
+// fileinclude = require('gulp-file-include'),// include 文件用
+//  template = require('gulp-template'),//替换变量以及动态html用
 // css
 gulp.task('css', function() {
-    return sass('src/**/*.scss', { style: 'expanded' })
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(utf8Convert())
-        .pipe(gulp.dest('dist/'))
-    .pipe(rename({ suffix: '.min' }))
-        .pipe(minifycss({
+    return sass('src/**/*.scss', { style: 'expanded' }) //以expand当时进行编译sass
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')) //给编译的sass添加前缀
+        .pipe(utf8Convert()) //转化成utf-8编码， 可以省略
+        .pipe(gulp.dest('dist/')) //保存到目标文件夹dist下
+        .pipe(rename({ suffix: '.min' })) //重命名
+        .pipe(minifycss({  //压缩css
             keepSpecialComments: 0
         }))
-        .pipe(gulp.dest('dist/'))
-        .pipe(notify({ message: 'css task complete' }));
+        .pipe(gulp.dest('dist/')) //保存
+        .pipe(notify({ message: 'css task complete' }))
+        .pipe(browserSync.stream()); //浏览器进行刷新
 });
 // js
 gulp.task('js', function() {
     return gulp.src('src/**/*.js')
-     //   .pipe(jshint())
-     //  .pipe(jshint.reporter('default')) // 输出检查结果
+        //   .pipe(jshint())
+        //  .pipe(jshint.reporter('default')) // 输出检查结果
         .pipe(gulp.dest('dist/js'))
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('dist/js'))
+        .pipe(concat('main.js')) //让所有的js压缩到main里面
+        .pipe(gulp.dest('dist/js')) //保存到js目录下
         .pipe(uglify()) //压缩
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/js'))
-        .pipe(notify({ message: 'js task complete' }));
+        .pipe(notify({ message: 'js task complete' }))
+        .pipe(browserSync.stream()); //浏览器进行刷新
 });
 // img
 gulp.task('img', function() {
@@ -67,7 +70,8 @@ gulp.task('img', function() {
             //  use: [pngquant()] //使用pngquant来压缩png图片
         })))
         .pipe(gulp.dest('dist/'))
-        .pipe(notify({ message: 'img task complete' }));
+        .pipe(notify({ message: 'img task complete' }))
+        .pipe(browserSync.stream()); //浏览器进行刷新
 });
 //html
 gulp.task('html', function() {
@@ -84,24 +88,29 @@ gulp.task('html', function() {
     gulp.src('src/html/*.html')
         .pipe(rev()) //版本控制
         .pipe(gulp.dest('dist/html'))
-        //  .pipe(htmlmin(options)) //压缩html文件
-        //  .pipe(gulp.dest('dist/'));
-        .pipe(notify({ message: 'html task complete' }));
+         .pipe(htmlmin(options)) //压缩html文件
+         .pipe(gulp.dest('dist/'))
+        .pipe(notify({ message: 'html task complete' }))
+        .pipe(browserSync.stream()); //浏览器进行刷新
 });
 // Clean
 gulp.task('clean', function(cb) {
     del(['dist/css', 'dist/js', 'dist/img', 'dist/html'], cb)
 });
 // Default task
-gulp.task('default', ['clean'], function() {
+gulp.task('default', ['watch'], function() {
     gulp.start('css', 'js', 'img', 'html');
 });
 // Watch
-gulp.task('watch', function() {
+gulp.task('watch', ["html", "css" , "js" , "img"], function() {
+    browserSync.init({
+            server : "./"
+        });
     gulp.watch('src/**/*.scss', ['css']);
     gulp.watch('src/**/*.js', ['js']);
     gulp.watch('src/img/*', ['img']);
     gulp.watch('src/**/*.html', ['html']);
-    livereload.listen();
-    gulp.watch(['dist/**']).on('change', livereload.changed);
+    // livereload.listen();
+    // gulp.watch(['dist/**']).on('change', livereload.changed);
+   gulp.watch(['dist/**']).on('change', browserSync.reload); //浏览器刷新
 });
